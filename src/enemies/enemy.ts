f4eaf7543e3b6d1543b323bf13d35887fec36390
dcaspit <dcaspit@gmail.com>
2025-08-26 17:28:30 +0300

    added health bar to enemy

--- src/enemies/enemy.ts
+++ src/enemies/enemy.ts
@@ -2,23 +2,55 @@ import r from "raylib";
 
 export default class Enemy {
   pos: r.Vector2 = { x: 0, y: 0 };
+  healthBar: r.Vector2 = { x: 0, y: 0};
   enemySize = 40;
   enemyTexture: r.Texture;
+  health = 20;
 
   constructor() {
     this.enemyTexture = r.LoadTexture('assets/enemy.png');
   }
 
+  takeDamage() {
+    this.health--;
+  }
+
   draw(position: r.Vector2) {
     this.pos.x = position.x + 5;
     this.pos.y = position.y + 5;
-    const destRect = {
+    this.healthBar.x = position.x - 5;
+    this.healthBar.y = position.y - 10;
+
+    this.drawEnemyTexture();
+    this.drawHealhBar();
+  }
+
+  drawHealhBar() {
+    const healthPercent = this.health / 20; // 20 == maxHealth
+    const bgRec: r.Rectangle = {
+      x: this.healthBar.x,
+      y: this.healthBar.y,
+      width: 55 * healthPercent,
+      height: 8
+    };
+    r.DrawRectangleRounded(bgRec, 0.5, 8, r.RED);
+    const linesRec: r.Rectangle = {
+      x: this.healthBar.x,
+      y: this.healthBar.y,
+      width: 55,
+      height: 7
+    };
+    r.DrawRectangleRoundedLines(bgRec, 0.5, 8, 1, r.BLACK);
+  }
+
+  drawEnemyTexture() {
+    const dest = {
       x: this.pos.x,
       y: this.pos.y,
       width: this.enemySize,
       height: this.enemySize,
     };
-    const sourceRect = {
+    const src = {
       x: 0,
       y: 0,
       width: this.enemyTexture.width,
@@ -26,8 +58,8 @@ export default class Enemy {
     };
     r.DrawTexturePro(
       this.enemyTexture,
-      sourceRect,
-      destRect,
+      src,
+      dest,
       { x: 0, y: 0 },
       0,
       r.WHITE,
